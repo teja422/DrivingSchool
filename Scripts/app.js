16d5ed24a4d7bd43c29bd674068c3b2c91dc4198
RAVI KUMAR <ravikumar533@gmail.com>
2015-09-14 11:52:42 +0530

    Driving School Project with Ashraw Full calendar feature

---
+++ Scripts/app.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    "use strict";
+
+    var app = angular.module("app", ["ui.calendar"]);
+
+    function myDriveController($scope) {
+        $scope.pageNumber = 1;
+        $scope.login = function() {
+            $scope.pageNumber += 1;
+        };
+        $scope.uiConfig = {
+            calendar: {
+                height: 450,
+                editable: true,
+                header: {
+                    left: 'month basicWeek basicDay agendaWeek agendaDay',
+                    center: 'title',
+                    right: 'today prev,next'
+                },
+                dayClick: $scope.alertEventOnClick,
+                eventDrop: $scope.alertOnDrop,
+                eventResize: $scope.alertOnResize
+            }
+        };
+    }
+
+    myDriveController.$inject = ["$scope"];
+    app.controller("MyDriveController", myDriveController);
+})();
